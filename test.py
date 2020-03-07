90e76deb6daa12af29126def65a486b46dd9096f
abhiram2600 <abhiram2600@users.noreply.github.com>
2020-03-07 22:07:42 +0530

    removed somethig

--- test.py
+++ test.py
@@ -5,16 +5,11 @@ def findnumber(s):
     las = s[k:]
     ss = las + fir
     return ss
+
+
 for _ in range(int(input())):
     n = int(input())
     s = input()
-    k=findnumber(s)
-    for i in range(k,len(ss)):
-        if ss[i]==s[i]:
-            pass
-        else:
-            if ascii(ss[i])>ascii(s[i]):
-
-                =findnumber(k,s)
+    k = findnumber(s)
     print(ss)
     print(k + 1)
